67dd3c4b70a92f7d5e8f8d46eb03c59984a3efca
19Urii91 <633mir@mail.ru>
2023-05-16 10:54:38 +0800

    Иправляет замечания наставника по скрипту

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,15 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toogle');
+const mainHeader = document.querySelector('.main-header');
+const mainNav= document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toogle');
 
-navMain.classList.remove('main-nav--nojs');
+mainHeader.classList.remove('main-header--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
   }
 });
