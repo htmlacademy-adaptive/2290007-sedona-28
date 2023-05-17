37463777fb62afcd303946d5e3f3497e5b875fdc
19Urii91 <633mir@mail.ru>
2023-05-17 11:56:55 +0800

    Готовит проект к защите

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 const mainHeader = document.querySelector('.main-header');
-const mainNav= document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toogle');
+const nav= document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toogle');
 
 mainHeader.classList.remove('main-header--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
   } else {
-    mainNav.classList.add('main-nav--closed');
-    mainNav.classList.remove('main-nav--opened');
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
   }
 });
